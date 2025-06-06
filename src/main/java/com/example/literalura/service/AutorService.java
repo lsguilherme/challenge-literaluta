051c5ce018512a4869fe7f5645a1661dcc4624ef
Guilherme Lacerda <83317386+lsguilherme@users.noreply.github.com>
2025-06-06 18:46:11 -0300

    Create: Listar autores registrados

--- src/main/java/com/example/literalura/service/AutorService.java
+++ src/main/java/com/example/literalura/service/AutorService.java
@@ -1,15 +1,42 @@
 package com.example.literalura.service;
 
+import com.example.literalura.model.Autor;
+import com.example.literalura.model.Livro;
+import com.example.literalura.repository.AutorRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AutorService {
 
+    private AutorRepository autorRepository;
+
+    public AutorService(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
+
+    @Transactional
     public void buscarAutoresRegistrados() {
-        System.out.println("Listando autores registrados");
+        List<Autor> autores = autorRepository.findAll();
+        autores.forEach(this::imprimirAutor);
     }
 
     public void buscarAutoresVivosEmUmAno() {
         System.out.println("Listando autores vivos em um determinado ano");
     }
+
+    private void imprimirAutor(Autor autor){
+        List<String> livros = autor.getLivros().stream().map(Livro::getTitulo).toList();
+        System.out.printf("""
+                Autor: %s
+                Ano de nascimento: %s
+                Ano de falecimento: %s
+                Livros: %s
+                
+                """, autor.getNome(), autor.getAnoNascimento(), autor.getAnoMorte(), livros);
+    }
+
 }
